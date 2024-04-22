d05bb16406d71174367953d6cf6863211e393140
ggreen96777 <112095202+ggreen96777@users.noreply.github.com>
2024-04-22 07:21:04 -0400

    Create install_dependencies.sh

---
+++ BONUS/scripts/install_dependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt update
+
+# Installing packages that the images may need such as nginx, git, and python3 for future coding and development.
+sudo apt install -y nginx python3 git
+
+# More configurations or Installments could be implemented here, but we can start with these for now.
